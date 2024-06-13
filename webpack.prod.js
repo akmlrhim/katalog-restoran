f4b929562cb69fb9d1b5681ff198a6f1476b2623
rahimmsz <akmalrahim376@gmail.com>
2024-06-13 12:35:29 +0800

    edit webpack.prod.js and add package for build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,10 @@
+/* eslint-disable import/order */
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -14,10 +19,41 @@ module.exports = merge(common, {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              cacheDirectory: true, // Enable caching
             },
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Clean up the dist folder before each build
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css", // Template name for the output CSS files
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true, // Enable parallel builds
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(), // Minimize CSS
+    ],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  cache: {
+    type: "filesystem", // Enable persistent caching
   },
 });
